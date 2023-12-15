9353b4c55e80089685deb1f893d4c442580e803a
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2023-12-15 07:29:34 +0530

    fix

--- resources/views/front/query/index.blade.php
+++ resources/views/front/query/index.blade.php
@@ -50,8 +50,8 @@
 					<div class="card mb-0">
 						<div class="card-body">
 							<div class="text-center mb-3">
-								<i class="icon-people icon-2x text-warning-400 border-warning-400 border-3 rounded-round p-3 mb-3 mt-1"></i>
-								<h5 class="mb-0">Admit Card System</h5>
+								<img src="{{asset("card-logo.png")}}" alt="">
+								<h5 class="mb-0">Assistant Section Officer <br>Special Recruitment Drive-2023</h5>
 								<span class="d-block text-muted">Send your query to us</span>
 							</div>
                             <label><strong>Name</strong></label>
